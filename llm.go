0cce18bf684e3e0a91132ef0e9305179a8e077a0
sti1l-0 <1737499156@qq.com>
2025-01-18 12:13:25 +0800

    更新配置文件；修复redis框架卡死闪退

--- llm.go
+++ llm.go
@@ -8,58 +8,59 @@ import (
 	"net/http"
 )
 
-func sendRequestAndGetResponse(apiKey string, redisServer *RedisServer) (string, error) {
-    // 构建请求数据
-    data := map[string]interface{}{
-        "model": "glm-4-flash",
-        "messages": redisServer.SessionList, // 使用 RedisServer 结构体的会话列表
-    }
-    fmt.Println(data)
+func sendRequestAndGetResponse(redisServer *RedisServer) (string, error) {
+	// 构建请求数据
+	data := map[string]interface{}{
+		"model":    redisServer.Config.Section("llm").Key("llm_model").String(),
+		"messages": redisServer.SessionList, // 使用 RedisServer 结构体的会话列表
+	}
+	fmt.Println(data)
 
-    jsonData, err := json.Marshal(data)
-    if err!= nil {
-        return "", err
-    }
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return "", err
+	}
 
-    // 发送请求
-    url := "https://open.bigmodel.cn/api/paas/v4/chat/completions"
-    req, err := http.NewRequest("POST", url, nil)
-    if err!= nil {
-        return "", err
-    }
+	// 发送请求
+	url := redisServer.Config.Section("llm").Key("llm_api_url").String()
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		return "", err
+	}
 
-    req.Header.Add("Authorization", "Bearer "+apiKey)
-    req.Header.Add("Content-Type", "application/json")
+	apiKey := redisServer.Config.Section("llm").Key("llm_api_key").String()
+	req.Header.Add("Authorization", "Bearer "+apiKey)
+	req.Header.Add("Content-Type", "application/json")
 
-    req.Body = ioutil.NopCloser(bytes.NewBuffer(jsonData))
+	req.Body = ioutil.NopCloser(bytes.NewBuffer(jsonData))
 
-    client := &http.Client{}
-    res, err := client.Do(req)
-    if err!= nil {
-        return "", err
-    }
-    defer res.Body.Close()
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
 
-    // 读取响应内容
-    body, err := ioutil.ReadAll(res.Body)
-    if err!= nil {
-        return "", err
-    }
+	// 读取响应内容
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
 
-    return string(body), nil
+	return string(body), nil
 }
 
 func extractMessageContent(response string) (string, error) {
-    fmt.Println(response)
-    var data map[string]interface{}
-    json.Unmarshal([]byte(response), &data)
+	fmt.Println(response)
+	var data map[string]interface{}
+	json.Unmarshal([]byte(response), &data)
 
-    choices := data["choices"].([]interface{})
-    if len(choices) > 0 {
-        choice := choices[0].(map[string]interface{})
-        message := choice["message"].(map[string]interface{})
-        return message["content"].(string),nil
-    }
+	choices := data["choices"].([]interface{})
+	if len(choices) > 0 {
+		choice := choices[0].(map[string]interface{})
+		message := choice["message"].(map[string]interface{})
+		return message["content"].(string), nil
+	}
 
-    return "",nil
+	return "", nil
 }
--- server.go
+++ server.go
@@ -17,16 +17,16 @@ import (
 )
 
 type SessionMessage struct {
-    Role    string `json:"role"`
-    Content string `json:"content"`
+	Role    string `json:"role"`
+	Content string `json:"content"`
 }
 
 type RedisServer struct {
-    server  *gev.Server
-    hashmap *hashmap.Map
-    Config  *ini.File
-    log     *logrus.Logger
-    SessionList []SessionMessage
+	server      *gev.Server
+	hashmap     *hashmap.Map
+	Config      *ini.File
+	log         *logrus.Logger
+	SessionList []SessionMessage
 }
 
 func NewRedisServer(address string, proto string, loopsnum int) (server *RedisServer, err error) {
@@ -50,7 +50,7 @@ func NewRedisServer(address string, proto string, loopsnum int) (server *RedisSe
 	}
 
 	// init llm info
-	updateSessionList(Serv,"请你扮演一个 redis 服务器，我将会以 redis 客户端的身份，通过命令行的形式与你沟通，你需要模仿真实 redis 服务器能够给出的响应向我回复。请注意，我只需要你给出命令响应，不需要任何其他的解释或分析。","system")
+	updateSessionList(Serv, "请你扮演一个 redis 服务器，我将会以 redis 客户端的身份，通过命令行的形式与你沟通，你需要模仿真实 redis 服务器能够给出的响应向我回复。请注意，我只需要你给出命令响应，不需要任何其他的解释或分析。", "system")
 	return Serv, nil
 }
 
@@ -98,11 +98,12 @@ func (s *RedisServer) OnMessage(c *connection.Connection, ctx interface{}, data 
 		out = []byte("+OK\r\n")
 	case "select":
 		out = []byte("+OK\r\n")
+	case "command":
+		out = []byte("+OK\r\n")
 	default:
-		apiKey := ""
 		updateSessionList(s, action, "user")
-		response, err := sendRequestAndGetResponse(apiKey,s)
-		if err!= nil {
+		response, err := sendRequestAndGetResponse(s)
+		if err != nil {
 			fmt.Println("请求出错:", err)
 			return
 		}
@@ -114,21 +115,21 @@ func (s *RedisServer) OnMessage(c *connection.Connection, ctx interface{}, data 
 
 		s.log.WithFields(logrus.Fields{
 			"llm response": extract_content,
-			"addr":   c.PeerAddr(),
+			"addr":         c.PeerAddr(),
 		}).Println()
 
-		out = []byte("+"+extract_content+"\r\n")
+		out = []byte("+" + extract_content + "\r\n")
 		updateSessionList(s, extract_content, "assistant")
 	}
 	return
 }
 
-func updateSessionList(rs *RedisServer, message string,role string) {
-    sessionMessage := SessionMessage{
-            Role:    role,
-            Content: message,
-        }
-        rs.SessionList = append(rs.SessionList, sessionMessage)
+func updateSessionList(rs *RedisServer, message string, role string) {
+	sessionMessage := SessionMessage{
+		Role:    role,
+		Content: message,
+	}
+	rs.SessionList = append(rs.SessionList, sessionMessage)
 }
 
 func (s *RedisServer) OnClose(c *connection.Connection) {
